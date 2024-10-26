20ff58252b09f77f69347b93c37d500c0269fc5f
fbernain <fbernain@it-bs.com.ar>
2024-10-26 16:02:27 -0300

    [ADD] modulos comunidad v17

--- odoo-argentina-ee/account_accountant_ux/models/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_followup_report
-from . import res_company
-from . import bank_rec_widget
-from . import account_move_line
-from . import account_move
